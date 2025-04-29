e6d4c568c28e1a54bdc0aedc689bb7044a0a15c1
JCardoso-UMAIA <A045146@umaia.pt>
2025-04-29 19:29:54 +0100

    react11

--- react_example_11-PassingState/js/clock.js
+++ react_example_11-PassingState/js/clock.js
@@ -1,1 +1,25 @@
-console.log("Create Clock component here.");
+class Clock extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { currentTime: new Date() };
+    this.launchClock();
+  }
+
+  launchClock() {
+    console.log(this);
+    setInterval(() => {
+      console.log('updating...');
+      this.setState({ currentTime: new Date() });
+    }, 1000);
+  }
+
+  render() {
+    console.log('Rendering...');
+    return React.createElement(
+      'div',
+      null,
+      React.createElement(AnalogDisplay, { time: this.state.currentTime.getTime() }),
+      React.createElement(DigitalDisplay, { time: this.state.currentTime.toLocaleString() })
+    );
+  }
+}
--- react_example_11-PassingState/js/script.js
+++ react_example_11-PassingState/js/script.js
@@ -1,1 +1,69 @@
-console.log("Add React.DOM instruction here.");
+console.log("Rendering the Clock component...");
+
+const DigitalDisplay = props => React.createElement(
+  "div",
+  null,
+  props.time
+);
+
+const AnalogDisplay = (props) => {
+  let date = new Date(props.time);
+  let dialStyle = {
+    position: 'relative',
+    top: 0,
+    left: 0,
+    width: 200,
+    height: 200,
+    borderRadius: 20000,
+    borderStyle: 'solid',
+    borderColor: 'black',
+  };
+  let secondHandStyle = {
+    position: 'absolute',
+    top: 100,
+    left: 100,
+    border: '1px solid red',
+    width: '40%',
+    height: 1,
+    transform: 'rotate(' + ((date.getSeconds() / 60) * 360 - 90).toString() + 'deg)',
+    transformOrigin: '0% 0%',
+    backgroundColor: 'red',
+  };
+  let minuteHandStyle = {
+    position: 'absolute',
+    top: 100,
+    left: 100,
+    border: '1px solid grey',
+    width: '40%',
+    height: 3,
+    transform: 'rotate(' + ((date.getMinutes() / 60) * 360 - 90).toString() + 'deg)',
+    transformOrigin: '0% 0%',
+    backgroundColor: 'grey',
+  };
+  let hourHandStyle = {
+    position: 'absolute',
+    top: 92,
+    left: 106,
+    border: '1px solid grey',
+    width: '20%',
+    height: 7,
+    transform: 'rotate(' + ((date.getHours() / 12) * 360 - 90).toString() + 'deg)',
+    transformOrigin: '0% 0%',
+    backgroundColor: 'grey',
+  };
+  return React.createElement(
+    'div',
+    null,
+    React.createElement(
+      'div',
+      { style: dialStyle },
+      React.createElement('div', { style: secondHandStyle }),
+      React.createElement('div', { style: minuteHandStyle }),
+      React.createElement('div', { style: hourHandStyle })
+    )
+  );
+};
+
+// Create a root and render the Clock component
+const root = ReactDOM.createRoot(document.getElementById('content'));
+root.render(React.createElement(Clock, null));
